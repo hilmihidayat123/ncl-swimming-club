a05ecd6fec5a1d115f2f96437f76c9266669a70c
Hilmi Hidayat <hilmihidayat54@gmail.com>
2026-04-26 18:50:36 +0700

    fix vite to static assets for production

--- resources/views/ncl/home.blade.php
+++ resources/views/ncl/home.blade.php
@@ -3,7 +3,8 @@
 
 <!-- HERO -->
 <head>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="{{ asset('build/assets/app.css') }}">
+<script src="{{ asset('build/assets/app.js') }}" defer></script>
 
     <meta charset="UTF-8">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
