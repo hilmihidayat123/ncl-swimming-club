610f20b7fe5155666ee9c716a736061fd4400bbe
hilmihidayat123 <hilmihidayat54@gmail.com>
2026-04-27 19:47:53 +0700

    Replace Vite with asset links for CSS and JS

--- resources/views/ncl/pengumuman.blade.php
+++ resources/views/ncl/pengumuman.blade.php
@@ -3,7 +3,8 @@
 @section('content')
 
 <head>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+      <link rel="stylesheet" href="{{ asset('build/assets/app-BmAVGTu2.css') }}">
+<script src="{{ asset('build/assets/app-CKl8NZMC.js') }}" defer></script>
     <meta charset="UTF-8">
     <title>Pengumuman</title>
     <script src="https://cdn.tailwindcss.com"></script>
