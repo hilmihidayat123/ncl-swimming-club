e7a3dfc0f14ffdf4610e2157b6a07870b41678af
hilmihidayat123 <hilmihidayat54@gmail.com>
2026-04-27 19:49:29 +0700

    Update jadwal.blade.php

--- resources/views/ncl/jadwal.blade.php
+++ resources/views/ncl/jadwal.blade.php
@@ -3,7 +3,8 @@
 @section('content')
 
 <head>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+      <link rel="stylesheet" href="{{ asset('build/assets/app-BmAVGTu2.css') }}">
+<script src="{{ asset('build/assets/app-CKl8NZMC.js') }}" defer></script>
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
